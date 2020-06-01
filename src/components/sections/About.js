82cf71558e6e01d4675c0725ae84268082969fe5
Sumit Haswar <sumithaswar@gmail.com>
2020-05-31 22:33:14 -0700

    update about-me (#5)

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -56,20 +56,22 @@ const About = () => (
             {/*</Art>*/}
             <div>
               <p>
-                I am originally from the city of Mumbai in India. I moved to US in 2013 for pursuing MS in Computer Science at California State University Long Beach (Go Beach!),
-                and for me California was love at first sight.
+                I am originally from the city of Mumbai in India. I moved to US in 2013 for pursuing MS in Computer Science
+                at California State University Long Beach (Go Beach!), and for me California was love at first sight.
               </p>
               <p>
-                Before moving to US, I spent couple of years in Vienna working for <a rel="noreferrer noopener" style={{ color: '#8bd8ed', textDecoration: 'inherit'}} href={'https://www.iaea.org/'} target={'_blank'}>IAEA</a> as a Software Engineer, and before that
-                I was developing financial and logistics software in my hometown of Mumbai. I've been working full-time for more than 10
-                years and I think the best aspect of programming is doing it as a team. I have been the best programmer
+                Before moving to US, I spent couple of years in Vienna working for <a rel="noreferrer noopener"
+                                                                                      style={{ color: '#8bd8ed', textDecoration: 'inherit'}}
+                                                                                      href={'https://www.iaea.org/'} target={'_blank'}>IAEA</a> as a Software Engineer,
+                and before that I was developing financial and logistics software in my hometown of Mumbai. I've been working full-time for more than 10
+                years and I think the best aspect of developing software is doing it as a team. I have been the best programmer
                 in some teams and in some I've been the worst. Both experiences together taught me so much which by itself would have taught me very little.
               </p>
               <br/>
               <p>
-                I am an avid soccer fan and a <a rel="noreferrer noopener" style={{ color: '#8bd8ed', textDecoration: 'inherit'}} href={'https://www.acmilan.com/en'} target={'_blank'}>Rossoneri</a> supporter. When I am not coding I play pick-up soccer, cycle around SF, play Dark Souls and listen to classic rock.
-                Some of my favorite bands are Led Zeppelin, Deep Purple and Pink Floyd.
-                I spend most of my money either on Onitsuka sneakers or on designbyhumans t-shirts, you'll mostly find me in either or both.
+                I am an avid soccer fan and a <a rel="noreferrer noopener" style={{ color: '#8bd8ed', textDecoration: 'inherit'}}
+                                                 href={'https://www.acmilan.com/en'} target={'_blank'}>Rossoneri</a> supporter.
+                In my free time I play pick-up soccer, cycle around SF, play Dark Souls and listen to classic rock.
               </p>
             </div>
           </Grid>
