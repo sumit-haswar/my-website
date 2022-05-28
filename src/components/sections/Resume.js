bc7d90af97f447413134b1fc64bcaf8fdb7815b5
Sumit Haswar <sumithaswar@gmail.com>
2022-05-28 11:13:19 -0700

    update resume section (#10)

--- src/components/sections/Resume.js
+++ src/components/sections/Resume.js
@@ -60,96 +60,90 @@ const Resume = () => (
              <del><a rel="noreferrer noopener" href={'https://www.practicefusion.com'} target={'_blank'}>Practice-Fusion</a></del>&nbsp;
              <del><a rel="noreferrer noopener" href={'https://www.minted.com'} target={'_blank'}>Minted</a></del>&nbsp;
              <a rel="noreferrer noopener" href={'https://www.zillowgroup.com/'} target={'_blank'}>Zillow Group</a> on distributed services,
-          web apis, functional programming and data retrieval systems.
+          web apis, functional programming and data retrieval systems using Python, FastAPI, Flink, Kafka, AWS Cloud Products and MS SQL.
+            Most of the work I do at Zillow is related to providing new,
+            <a rel="noreferrer noopener" href={'https://www.zillowgroup.com/news/how-scientists-at-zillow-are-making-your-home-shopping-journey-faster-and-more-accurate/'} target={'_blank'}> immersive and interactive home tour experiences </a>
+            to our users making their home shopping journey faster and more accurate.
           </p>
           <h3>Technical Skills</h3>
-          <Grid inverse>
-            <div>
-              <p>
-                  Programming Languages:<br/>
-                  Web Technologies:<br/>
-                  Other Technologies:<br/>
-                  Databases & Storage:
-              </p>
-            </div>
-            <div>
-              <p>
-                Python, Java, C#, JavaScript<br/>
-                GraphQL, REST, AWS<br/>
-                Apache Flink, Git, Docker, Kafka<br/>
-                MS SQL Server, MySQL, PostgreSQL, S3, MongoDB
-              </p>
-            </div>
-          </Grid>
+          <Table>
+          <table cellPadding="4" cellSpacing="4">
+            <tbody>
+            <tr>
+              <td>
+                <h4>Programming Languages</h4>
+              </td>
+              <td>
+                Python, Java, C#, JavaScript
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Web Technologies</h4>
+              </td>
+              <td>
+                GraphQL, REST, AWS, Kubernetes
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Other Technologies</h4>
+              </td>
+              <td>
+                Apache Flink, Git, Docker, Kafka
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Databases & Storage</h4>
+              </td>
+              <td>
+                MS SQL Server, PostgreSQL, S3, DynamoDB
+              </td>
+            </tr>
+            </tbody>
+          </table>
+          </Table>
+
           <h3>Education</h3>
-          <Grid oneColumn>
-            <div>
-              <p>California State University, Long Beach</p>
-              <p>&ensp; MS, Full-Time Program in Computer Science</p>
-              <p>&ensp; Research Project: <a rel="noreferrer noopener" href={constraintModelingPdf} target={'_blank'}>Platform Agnostic Constraint Modeling using XML</a></p>
-              <br/>
-              <p>University of Mumbai, Bachelor of Engineering in Computer Science</p>
-            </div>
-          </Grid>
+          <Table>
+          <table cellPadding="4" cellSpacing="4">
+            <tbody>
+            <tr>
+              <td><h4>MS</h4></td>
+              <td>Computer Science</td>
+              <td><a target={'_blank'} href={'https://www.csulb.edu/'}>California State University, Long Beach</a></td>
+            </tr>
+            {/*<tr>*/}
+            {/*  <td colSpan={3}>Research Project: <a rel="noreferrer noopener" href={constraintModelingPdf} target={'_blank'}>Platform Agnostic Constraint Modeling using XML</a></td>*/}
+            {/*</tr>*/}
+            <tr>
+              <td><h4>BE</h4></td>
+              <td>Computer Engineering</td>
+              <td><a target={'_blank'} href={'https://mu.ac.in/'}>University of Mumbai</a></td>
+            </tr>
+            </tbody>
+          </table>
+          </Table>
         </Container>
       </Section>
     )}
   />
 );
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 2fr;
-  grid-gap: 0px;
+const Table = styled.table`
   text-align: left;
-  align-items: left;
-  justify-items: left;
-  margin: 0px 0;
-
-  ${props =>
-  props.inverse &&
-  `
-    text-align: left;
-    grid-template-columns: 1fr 3fr;
-  `}
-
-  ${props => props.oneColumn &&
-  `
-    text-align: left;
-    grid-template-columns: 3fr 1fr;
-  `}
-
-  h2 {
-    margin-bottom: 16px;
+  margin: 0 0;
+  color: ${props => props.theme.color.black.light};
+  
+  td h4 {
+    font-weight: bold;
+    margin-top: 0;
+    margin-bottom: 0;
+    display: inline;
   }
   
-  h3 {
-    margin-bottom: 16px;
-  }
-
-  @media (max-width: ${props => props.theme.screen.md}) {
-    grid-template-columns: 1fr;
-    text-align: left;
-    margin-bottom: 96px;
-
-    &:last-child {
-      margin-bottom: 24px;
-    }
-
-    ${props =>
-  props.inverse &&
-  `
-        ${Art} {
-          order: 2;
-        }
-    `}
-  }
 `;
 
-const Art = styled.figure`
-  // margin: 0;
-  max-width: 380px;
-  width: 100%;
-`;
 
 export default Resume;
